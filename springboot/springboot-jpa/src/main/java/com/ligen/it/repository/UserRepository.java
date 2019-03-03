0971cf087d366c7a2f3d09fdeb4c4c765ce38a4c
ligen520 <741773200@qq.com>
2019-03-03 20:33:11 +0800

    55555

--- springboot/springboot-jpa/src/main/java/com/ligen/it/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ligen.it.repository;
-
-import com.ligen.it.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
